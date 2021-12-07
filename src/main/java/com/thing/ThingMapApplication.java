8f76a901608ff23a4d276c70569cb5cbcad17108
yui2070 <yui2070@naver.com>
2021-12-07 16:05:50 +0900

    프로젝트 업로드

---
+++ src/main/java/com/thing/ThingMapApplication.java
@@ -0,0 +1,13 @@
+package com.thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thing/ThingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
